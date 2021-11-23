70f30f9ba3ae4e96c7c53efa8e00a4d084f5dc64
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-11-22 23:40:20 -0300

    CSL change msg

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ inquirer
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
     } else {
-      // Something else went wrong
     }
   });
